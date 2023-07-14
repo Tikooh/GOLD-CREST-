79b84e0c26c0acfbd4274aaf0cd112d2afea8024
Tikooh <114261455+Tikooh@users.noreply.github.com>
2023-07-14 16:29:16 +0100

    Add files via upload

--- angles v3.py
+++ angles v3.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import torch
-
-#model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 class drone:
     def __init__(self, x, y):
@@ -41,61 +39,43 @@ def ang3Points(coords):
         print(f"Angle: {angle}")
 
 if __name__ == "__main__":
-    plt.rcParams["figure.figsize"] = [500, 500] #defines matplotlib figure size
-    plt.rcParams["figure.autolayout"] = True
-
-    im = "mouse.jpg"
+    #model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    matplotlib.use('TkAgg')
+    im = ("drone1.png", "drone2.png", "drone3.jpg", "drone4.jpeg")
 
     # Inference
     results = model(im)
-
     # Results
     results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
 
-    results.xyxy[0]  # im predictions (tensor)
-    initialCoords = results.pandas().xyxy[0]  # im predictions (pandas)
+    locationList = []
 
-    x_centre = np.array([(initialCoords['xmin'][0] + initialCoords['xmax'][0]) / 2])
-    y_centre = np.array([(initialCoords['ymin'][0] + initialCoords['ymax'][0]) / 2])
+    for i in range(len(results)):
+        results.xyxy[i]  # im predictions (tensor)
+        initialCoords = results.pandas().xyxy[i]  # im predictions (pandas)
+        print(initialCoords)
 
+        x_centre = np.array([(initialCoords['xmin'][0] + initialCoords['xmax'][0]) / 2])
+        y_centre = np.array([(initialCoords['ymin'][0] + initialCoords['ymax'][0]) / 2])
+        print(type(x_centre))
 
-    drone = drone(x_centre, y_centre)
+        locationList.append(drone(x_centre, y_centre))
 
     coords =[]
+    # plt.rcParams["figure.figsize"] = [500, 500] #defines matplotlib figure size
+    # plt.rcParams["figure.autolayout"] = True    
+    for index, i in enumerate(locationList):
         
-    #plot the points on the graph
+        #plot the points on the graph
+        plt.plot(i.x, i.y, 'r*')
+        print(i.x)
+        #zip the x and y coordinates into a tuple
+        for xy in zip(i.x, i.y):
+            coords.append(xy)
+            plt.annotate(f'drone {index}', xy=xy)
 
-    print(f"y = {drone.y} x = {drone.x}")
-    plt.plot(drone.x, drone.y, 'r*')
-
-    #zip the x and y coordinates into a tuple
-    for xy in zip(drone.x, drone.y):
-        coords.append(xy)
-        plt.annotate(f'drone 1', xy=xy)
     
     linesegments(coords)
     ang3Points(coords)
     plt.show()
-
-
-    
-
-
-
-
-
-
-    # locationList = [drone(np.random.rand(1), np.random.rand(1)) for i in range(5)] #random locations
-
-    # coords =[]
-    # for index, i in enumerate(locationList):
-        
-    #     #plot the points on the graph
-    #     plt.plot(i.x, i.y, 'r*')
-    #     #zip the x and y coordinates into a tuple
-    #     for xy in zip(i.x, i.y):
-    #         coords.append(xy)
-    #         plt.annotate(f'drone {index}', xy=xy)
-
-    # linesegments(coords)
-    # ang3Points(coords)
