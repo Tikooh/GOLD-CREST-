4bcac65b443325566e6aec2def3640e2342ae9c7
Ticoo <JLRGeorge.Lo@gmail.com>
2023-09-01 12:05:11 +0100

    Prep for later

--- PreditionEdgeTPU.py
+++ PreditionEdgeTPU.py
@@ -2,6 +2,11 @@ from edgetpumodel import EdgeTPUModel
 from prediction import predictor
 from prediction import drone
 from utils import get_image_tensor
+import threading
+import concurrent.futures
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+from Controller import increase_speed, decrease_speed, rotate
 
 import cv2
 import yaml
@@ -12,6 +17,23 @@ import pandas
 import numpy as np
 
 if __name__ == "__main__":
+
+    direction = [22, 17]
+    step = [23, 18]
+    en = [24, 27]
+    msx = [(26, 21, 20), (16, 19, 13)]
+
+    motors = [RpiMotorLib.A4988Nema(direction[i], step[i], msx[i], "DRV8825") for i in range(2)]
+
+    GPIO.setup(en[0],GPIO.OUT)
+    GPIO.setup(en[1],GPIO.OUT)
+    GPIO.output(en[0],GPIO.LOW)
+    GPIO.output(en[1],GPIO.LOW)
+
+    LEFT, RIGHT, UP, DOWN = False, False, False, False
+    SPEED = 4
+    SPEEDS = ["Full", "Half", "1/4", "1/8", "1/16", "1/32"]
+
     model = EdgeTPUModel()
     location_pred = predictor([],[],[])
     input_size = model.get_image_size()
@@ -46,39 +68,21 @@ if __name__ == "__main__":
                     predictor.xlist.append(x_centre)
                     predictor.ylist.append(y_centre)
                     currentFrame += 1
+
+                    if x_centre
+
+                    rotate()
                 
                 else:
+                    #TODO add motor controls
                     locationList = predictor.getLocationList(xlist, ylist, tlist)
                     predX, predY, predT = predictor.getPredictions(locationList)
                     predictor.tlist = predictor.tlist[:-1]
                     predictor.xlist = predictor.xlist[:-1]
                     predictor.ylist = predictor.ylist[:-1]
-                    
+
+                    rotate()
         except KeyboardInterrupt:
             break
 
         cam.release()
-
-            # ret,frame = im.read()
-            # # print(f"ret output: {ret}, frame output: {frame}")
-
-            # if ret:
-            #     results = model(frame)
-            #     # Results
-            #     results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
-            #     try:
-                    
-            #         for i in range(len(results)):
-            #             results.xyxy[i]  # im predictions (tensor)
-            #             initialCoords = results.pandas().xyxy[i]  # im predictions (pandas)
-                        
-            #             x_centre = np.array([(initialCoords['xmin'][0] + initialCoords['xmax'][0]) / 2])
-            #             y_centre = np.array([(initialCoords['ymin'][0] + initialCoords['ymax'][0]) / 2])
-
-            #             locationList.append(drone(x_centre, y_centre))
-            #     except:
-            #         print("Error")
-                    
-            #     cv2.imwrite(f"image{currentframe}.png",frame) #creates the image
-            #     draw(f"image{currentframe}.png",initialCoords) #draws the box
-            #     currentframe += 1
